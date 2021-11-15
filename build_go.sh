9dae2e4675acd852e75621af3bde4e2ad5d239da
Lawliet-Chan <1576710154@qq.com>
2021-11-15 10:48:40 +0800

    update protoc

--- build_go.sh
+++ build_go.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# protoc-gen-go v1.27.1-devel
-# protoc        v3.13.0
+# protoc-gen-go v1.27.1
+# protoc        v3.17.3
 
 protoc  -I ./ \
---go_out=plugins=grpc:. ./*.proto
+--go_out=. ./*.proto
+
+protoc  -I ./ \
+--go-grpc_out=. ./*.proto
 
 mv ./goproto ../yu/types
