ba8e11d9a56af249562b168fc2d2d34d19529a55
Lawliet-Chan <1576710154@qq.com>
2022-11-28 23:09:17 +0800

    statedb

--- build_go.sh
+++ build_go.sh
@@ -7,7 +7,7 @@ protoc  -I ./ \
 --go_out=. ./*.proto
 
 protoc  -I ./ \
---go-grpc_out=. ./*.proto
+--go-grpc_out=. --go-grpc_opt=require_unimplemented_servers=false ./*.proto
 
 rm -r ../yu/core/types/goproto
 mv ./goproto ../yu/core/types
