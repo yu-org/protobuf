41d5194a05adce93e14bae653c9901d8ee44e21e
Lawliet-Chan <1576710154@qq.com>
2024-09-11 23:51:55 +0800

    rm grpc

--- build_go.sh
+++ build_go.sh
@@ -6,8 +6,8 @@
 protoc  -I ./ \
 --go_out=. ./*.proto
 
-protoc  -I ./ \
---go-grpc_out=. --go-grpc_opt=require_unimplemented_servers=false ./*.proto
+#protoc  -I ./ \
+#--go-grpc_out=. --go-grpc_opt=require_unimplemented_servers=false ./*.proto
 
 rm -r ../yu/core/types/goproto
 mv ./goproto ../yu/core/types
